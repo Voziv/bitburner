80151a2700c96822eb492ea366401a1c932ac351
Lee Robert <25726+Voziv@users.noreply.github.com>
2025-12-07 17:46:10 -0500

    feat: better init script

--- src/init.ts
+++ src/init.ts
@@ -23,29 +23,109 @@ const hosts = [
     ],
 ];
 
+const milestones = {
+    hackZero: false,
+    hackOne: false,
+    hackTwo: false,
+    hackThree: false,
+    hackFour: false,
+    hackFive: false,
+    purchasedServers: 0,
+}
+
+const tools = [
+    'BruteSSH.exe',
+    'FTPCrack.exe',
+    'relaySMTP.exe',
+    'HTTPWorm.exe',
+    'SQLInject.exe',
+];
+
 export async function main(ns: NS): Promise<void> {
 
     // Can't use singularity yet. Not sure when it gets unlocked or whether it's permanent or not.
     // ns.singularity.universityCourse(ns.enums.LocationName.Sector12RothmanUniversity, ns.enums.UniversityClassType.computerScience, true)
 
+    while (true) {
+        if (
+            milestones.purchasedServers >= ns.getPurchasedServerLimit()
+            && milestones.hackOne
+            && milestones.hackTwo
+        ) {
+            break;
+        }
 
-    for (const host of hosts[0]) {
-        const threads = Math.floor(ns.getServerMaxRam(host) / ns.getScriptRam(script));
-        ns.scp(script, host);
-        ns.nuke(host);
-        ns.killall(host);
-        ns.exec(script, host, threads, target);
+        let toolCount = countTools(ns);
+
+        if (!milestones.hackZero) {
+            hack(ns, hosts[0]);
+            milestones.hackZero = true;
+        } else if (!milestones.hackOne && toolCount >= 1) {
+            hack(ns, hosts[1]);
+            milestones.hackOne = true;
+        } else if (!milestones.hackTwo && toolCount >= 2) {
+            hack(ns, hosts[2]);
+            milestones.hackTwo = true;
+        } else if (!milestones.hackThree && toolCount >= 3) {
+            hack(ns, hosts[3]);
+            milestones.hackThree = true;
+        } else if (!milestones.hackFour && toolCount >= 4) {
+            hack(ns, hosts[4]);
+            milestones.hackFour = true;
+        } else if (!milestones.hackFive && toolCount >= 5) {
+            hack(ns, hosts[5]);
+            milestones.hackFive = true;
+        }
+
+        if (milestones.purchasedServers >= ns.getPurchasedServerLimit()) {
+            purchaseServers(ns);
+        }
+
+        await ns.sleep(1000);
+    }
+}
+
+function countTools(ns: NS) {
+    let toolCount = 0;
+
+
+    for (const tool of tools) {
+        if (ns.fileExists(tool)) {
+            toolCount++;
+        }
     }
 
-    while (!ns.fileExists('BruteSSH.exe')) {
-        await ns.sleep(60000);
-    }
+    return toolCount;
+}
 
-    for (const host of hosts[1]) {
+function purchaseServers(ns: NS) {
+    const script = "money.js"
+    const target = "joesguns"
+    const ram = 8;
+
+    // Check if we have enough money to purchase a server
+    if (ns.getServerMoneyAvailable("home") > ns.getPurchasedServerCost(ram)) {
+        const serverNumber = `${milestones.purchasedServers + 1}`.padStart(2, '0');
+        let hostname = ns.purchaseServer("voz-drone-" + serverNumber, ram);
+        ns.scp(script, hostname);
+        ns.exec(script, hostname, 3, target);
+        ++milestones.purchasedServers;
+    }
+}
+
+function useTools(ns: NS, target: string) {
+    for (const tool of tools) {
+        if (ns.fileExists(tool)) {
+            ns.exec(tool, target);
+        }
+    }
+}
+
+function hack(ns: NS, hosts: string[]) {
+    for (const host of hosts) {
         const threads = Math.floor(ns.getServerMaxRam(host) / ns.getScriptRam(script));
         ns.scp(script, host);
-        ns.brutessh(host);
-        ns.nuke(host);
+        useTools(ns, host);
         ns.nuke(host);
         ns.killall(host);
         ns.exec(script, host, threads, target);
